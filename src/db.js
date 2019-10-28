d31e44830dcf078962cff609b05263200f7a7bf4
GregTCLTK <gregtcltk@gmail.com>
2019-10-28 19:34:16 +0100

    DELETE!!!

--- src/db.js
+++
@@ -1,6 +0,0 @@
-const r = require('rethinkdbdash')({
-  port: 28015,
-  host: "127.0.0.1"
-});
-
-module.exports = r;
--- src/routers/index.js
+++
@@ -1,3 +0,0 @@
-const express = require('express');
-const csrfM = require('./csrf');
-const r = require('./../db');
