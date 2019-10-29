c67006077f2df4add4f9a33747ce09b05918f232
GregTCLTK <gregtcltk@gmail.com>
2019-10-29 17:37:29 +0100

    Update .travis.yml

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+set_include_path('/var/www/vhosts/bigbotnetwork.wh.hostnation.de/httpdocs/Hadder/');
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require "discord.php";
+require "functions.php";
+
+init("", "http://bigbotnetwork.de/Hadder/login.php", "637002314162372639", "");
+
+get_user();
+
+$_SESSION['guilds'] = get_guilds();
+
+redirect("http://bigbotnetwork.de/Hadder/index.php");
+?>
+
+
