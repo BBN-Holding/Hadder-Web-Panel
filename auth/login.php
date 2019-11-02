1476b5bece1f5fd1e8ce7865e6cba66116e06362
GregTCLTK <gregtcltk@gmail.com>
2019-11-02 17:29:25 +0100

    Logging

--- auth/login.php
+++ auth/login.php
@@ -13,5 +13,70 @@ get_user();
 
 $_SESSION['guilds'] = get_guilds();
 
+
+$url = "";
+
+$hookObject = json_encode([
+
+    "username" => "Web Panel Logger",
+    "avatar_url" => "https://bigbotnetwork.de/img/Hadder.png",
+    "tts" => false,
+
+    "embeds" => [
+        [
+            "title" => "New Login",
+            "type" => "rich",
+            "description" => "New User logged in",
+            "timestamp" => date('Y-m-d\TH:i:s.Z\Z', time()),
+            "color" => hexdec("2F5E69"),
+
+            "footer" => [
+                "text" => "Web Panel Logger",
+                "icon_url" => "https://bigbotnetwork.de/img/Hadder.png"
+            ],
+
+            "thumbnail" => [
+                "url" => "https://cdn.discordapp.com/avatars/" .  $_SESSION['user_id'] . "/" . $_SESSION['user_avatar']
+            ],
+
+            "fields" => [
+                [
+                    "name" => "User",
+                    "value" => $_SESSION['username'] . '#' . $_SESSION['discrim'],
+                    "inline" => false
+                ],
+                [
+                    "name" => "ID",
+                    "value" => $_SESSION['user_id'],
+                    "inline" => true
+                ],
+                [
+                    "name" => "E-Mail",
+                    "value" => $_SESSION['email'],
+                    "inline" => true
+                ]
+            ]
+        ]
+    ]
+
+], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+
+$ch = curl_init();
+
+curl_setopt_array($ch, [
+    CURLOPT_URL => $url,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $hookObject,
+    CURLOPT_HTTPHEADER => [
+        "Length" => strlen($hookObject),
+        "Content-Type" => "application/json"
+    ]
+]);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+
+
 redirect("http://bigbotnetwork.de/Hadder/index.php");
 ?>
