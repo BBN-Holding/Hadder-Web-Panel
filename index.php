0c22cef3e833e69e89027f8d52378a30104557e6
GregTCLTK <gregtcltk@gmail.com>
2019-11-14 19:09:12 +0100

    Broke everything
    Hopefully fixed soon xD

--- index.php
+++ index.php
@@ -40,11 +40,12 @@ require "auth/discord.php";
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
         <noscript><link rel="stylesheet" href="css/noscript.css" /></noscript>
 
-        <link rel="stylesheet" href="css/main.css?v=1"/>
+        <?php
+        echo "<link rel=\"stylesheet\" href=\"css/main.css?v=" . rand() .  "\"/>";
+        ?>
         <link rel="stylesheet" href="css/materialize.css">
 
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
     </head>
 
     <body class="is-preload">
